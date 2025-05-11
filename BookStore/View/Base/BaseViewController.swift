e6047805558bf14d8e220edabd355dd4e9fa1fd5
NH <mnh4140@gmail.com>
2025-05-11 23:36:32 +0900

    [Feat] 베이스 뷰 생성

--- BookStore/View/Base/BaseViewController.swift
+++ BookStore/View/Base/BaseViewController.swift
@@ -11,6 +11,9 @@ class BaseViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+        setConstraints()
     }
     
     func setUI() {
