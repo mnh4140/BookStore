fb94ad2331cdc79343f29ee6a4195d8541a9e514
NH <mnh4140@gmail.com>
2025-05-11 23:32:41 +0900

    [Feat] 검색 View 기능 구현

--- BookStore/View/SearchTabViewController.swift
+++ BookStore/View/SearchTabViewController.swift
@@ -7,13 +7,130 @@
 
 import UIKit
 
-class SearchTabViewController: BaseViewController {
+final class SearchTabViewController: BaseViewController {
+    private let searchBar = UISearchBar()
+    private lazy var resultCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createCompositionalLayout())
+        collectionView.backgroundColor = .red
+        collectionView.register(ResultCell.self, forCellWithReuseIdentifier: String(describing: ResultCell.self))
+        collectionView.register(ResultHeaderCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: String(describing: ResultHeaderCell.self))
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    private var data: [BookData.Documents] = []
+    
+    let book = BookListViewModel()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        bind()
+    }
+    
     override func setUI() {
         super.setUI()
         
+        view.addSubview(searchBar)
+        view.addSubview(resultCollectionView)
     }
     
     override func setConstraints() {
         super.setConstraints()
+        
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
+        }
+        
+        resultCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.searchBar.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(100)
+        }
+    }
+    
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            switch sectionIndex {
+            case 0: // 최근 책
+                return self.createDefaultLayout()
+            case 1: // 검색 결과
+                return self.createDefaultLayout()
+            default:
+                return self.createDefaultLayout()
+            }
+        }
+        return layout
+    }
+    
+    private func createDefaultLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        item.contentInsets = .init(top: 20, leading: 8, bottom: 0, trailing: 8)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(80)
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.contentInsets = .init(top: 4, leading: 0, bottom: 0, trailing: 0)
+        
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(50))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize,
+                                                                 elementKind: UICollectionView.elementKindSectionHeader,
+                                                                 alignment: .top)
+
+        section.boundarySupplementaryItems = [header]
+
+        return section
+    }
+    
+    private func bind() {
+        // 데이터 바인딩
+        book.fetchBookList(query: "미움 받을 용기") { [weak self] books in
+            guard let self else { return }
+            self.data = books
+            DispatchQueue.main.async {
+                self.resultCollectionView.reloadData()
+            }
+        }
+    }
+}
+
+extension SearchTabViewController: UICollectionViewDelegate {
+    
+}
+
+extension SearchTabViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ResultCell.self), for: indexPath) as? ResultCell else { return .init() }
+        
+        cell.configure(data: data[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: String(describing: ResultHeaderCell.self), for: indexPath) as? ResultHeaderCell else {
+                return UICollectionReusableView()
+            }
+            header.configure(text: "검색 결과")
+            return header
+        }
+        return UICollectionReusableView()
     }
 }
