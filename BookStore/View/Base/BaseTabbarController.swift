d0b4630f862cfd0355a1385a340bea713955f024
NH <mnh4140@gmail.com>
2025-05-11 15:44:15 +0900

    [Feat] 베이스 컨트롤러 생성

---
+++ BookStore/View/Base/BaseTabbarController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseTabbarController.swift
+//  BookStore
+//
+//  Created by NH on 5/11/25.
+//
+
+import UIKit
+
+class BaseTabbarController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setTabbBar() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+}
---
+++ BookStore/View/Base/BaseViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseViewController.swift
+//  BookStore
+//
+//  Created by NH on 5/11/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func setUI() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+}
