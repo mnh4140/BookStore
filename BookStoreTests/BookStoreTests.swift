95314b9b938141567859e355f87162b412bb3baf
NH <mnh4140@gmail.com>
2025-05-11 14:47:36 +0900

    Initial Commit

---
+++ BookStoreTests/BookStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreTests.swift
+//  BookStoreTests
+//
+//  Created by NH on 5/11/25.
+//
+
+import Testing
+@testable import BookStore
+
+struct BookStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
