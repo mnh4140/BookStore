4d5b04b2e1ea67fc910cd021a10458a559ace2f6
NH <mnh4140@gmail.com>
2025-05-12 20:40:21 +0900

    [Feat] #2 검색 결과 Cell 개발 완료

--- BookStore/View/BookSearch/ResultCell.swift
+++ BookStore/View/BookSearch/ResultCell.swift
@@ -7,30 +7,55 @@
 
 import UIKit
 
-final class ResultCell: BaseCell {
-    private let name: UILabel = {
-        let label = UILabel()
-        label.text = "책 이름"
-        label.textColor = .black
-        label.font = .systemFont(ofSize: 18, weight: .medium)
-        return label
-    }()
+final class ResultCell: BaseCollectionViewCell {
+    private let name = UILabel()
     private let author = UILabel()
     private let price = UILabel()
+    private let stackView = UIStackView()
     
     override func setUI() {
         //contentView.backgroundColor = .green
-        contentView.addSubview(name)
+        
+        name.text = "책 이름"
+        name.textColor = .black
+        name.font = .systemFont(ofSize: 18, weight: .medium)
+        
+        author.text = "저자"
+        author.textColor = .lightGray
+        author.font = .systemFont(ofSize: 14, weight: .medium)
+        
+        price.text = "책 가격"
+        price.textColor = .black
+        price.textAlignment = .right
+        price.font = .systemFont(ofSize: 18, weight: .medium)
+        
+        stackView.axis = .horizontal
+        stackView.spacing = 16
+        stackView.distribution = .fill
+        [name, author, price].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        contentView.addSubview(stackView)
+        
     }
     
     override func setConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
         name.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(8)
-            make.centerY.equalToSuperview()
+            make.width.equalTo(stackView).multipliedBy(1.0 / 2.0)
+        }
+        
+        author.snp.makeConstraints { make in
+            make.width.equalTo(price).multipliedBy(1.0 / 2.0)
         }
     }
     
     func configure(data: BookData.Documents) {
         name.text = data.title
+        author.text = data.authors.joined(separator: ",")
+        price.text = String(data.price.formattedWithComma) + "원"
     }
 }
