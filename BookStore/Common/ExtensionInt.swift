8c860254f560f5399fc472e41f15aad9347ea3e0
NH <mnh4140@gmail.com>
2025-05-12 20:39:39 +0900

    [Feat] #2 가격 콤마 넣는 기능 추가

---
+++ BookStore/Common/ExtensionInt.swift
@@ -0,0 +1,17 @@
+//
+//  ExtensionInt.swift
+//  BookStore
+//
+//  Created by NH on 5/12/25.
+//
+
+import Foundation
+
+extension Int {
+    var formattedWithComma: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        return formatter.string(from: NSNumber(value: self)) ?? "\(self)"
+    }
+}
