a7f55cdcc429c88f9ad4d52adfa3c8a6287ce18a
NH <mnh4140@gmail.com>
2025-05-12 20:34:54 +0900

    [Feat] Base Class 개발

---
+++ BookStore/View/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseCell.swift
+//  BookStore
+//
+//  Created by NH on 5/11/25.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        
+    }
+    
+    func setConstraints() {
+
+    }
+}
---
+++ BookStore/View/Base/BaseTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseTableViewCell.swift
+//  BookStore
+//
+//  Created by NH on 5/12/25.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        
+    }
+    
+    func setConstraints() {
+
+    }
+}
