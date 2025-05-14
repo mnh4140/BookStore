eeae034340d28a0120e601c62389df5f03e56769
NH <mnh4140@gmail.com>
2025-05-14 12:38:09 +0900

    [Feat] #3 무한 스크롤 기능을 위한 비즈니스 로직 수정

--- BookStore/ViewModel/BookListViewModel.swift
+++ BookStore/ViewModel/BookListViewModel.swift
@@ -12,19 +12,39 @@ class BookListViewModel {
     init() {}
     
     /// ViewModel 에서 수행해야될 비즈니스 로직
-    func fetchBookList(query: String, completion: @escaping ([BookData.Documents]) -> Void) {
+    func fetchBookList(query: String, page: Int = 1, completion: @escaping ([BookData.Documents]) -> Void) {
 
         var components = URLComponents(string: "https://dapi.kakao.com/v3/search/book")
         
         components?.queryItems = [
-            URLQueryItem(name: "query", value: query)
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "page", value: "\(page)")
+            
         ]
         
         guard let url = components?.url else { return }
+        print("검색 결과 최종 요청 URL: \(url)")
         
         NetworkManager.shared.fetchData(url: url) { (result: BookData?) in
             guard let books = result?.documents else { return }
             completion(books)
+        }
+    }
+    
+    func fetchMetaData(query: String, page: Int = 1, completion: @escaping (BookData.Meta) -> Void) {
+        var components = URLComponents(string: "https://dapi.kakao.com/v3/search/book")
+        
+        components?.queryItems = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "page", value: "\(page)")
+        ]
+        
+        guard let url = components?.url else { return }
+        print("Meta 최종 요청 URL: \(url)")
+        
+        NetworkManager.shared.fetchData(url: url) { (result: BookData?) in
+            guard let meta = result?.meta else { return }
+            completion(meta)
         }
     }
 }
